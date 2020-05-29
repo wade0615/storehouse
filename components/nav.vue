37aa17cd2eb9f2f171ac685dd4a9ab9c34d7f90b
Wade <wsw0615@gmail.com>
2020-05-29 16:27:47 +0800

    feat: nav sidebar

--- components/nav.vue
+++ components/nav.vue
@@ -1,16 +1,28 @@
 <template>
-    <div class="example">
+    <header class="logo">
         <h1>{{ msg }}</h1>
-    </div>
+        <button class="active_sidebar" v-on:click='activeSidebar()'><i class="fas fa-bars"></i></button>
+        <div class="side_bar" :class="{active: isActive}" v-on:click='closeSidebar()'></div>
+    </header>
 </template>
 
 <script>
 export default {
     data () {
         return {
-            msg: `I'll be the navbar.`
+            msg: `Logo`,
+            isActive: false
         }
-    }
+    },
+    methods: {
+        activeSidebar(){
+            console.log('your active');
+            this.isActive = true
+        },
+        closeSidebar(){
+            this.isActive = false
+        }
+    },
 }
 </script>
 
