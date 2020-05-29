2d6fb0b2376d0682bbc6c1ac723093dd287d41dc
Wade <wsw0615@gmail.com>
2020-05-29 12:01:26 +0800

    feat: @vue/cli @vue/cli-service-global vue-loader vue-template-compiler

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 
 const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
     entry: "./app.js", //引入的檔案
@@ -7,5 +8,16 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist'), //輸出的路徑
         filename: "build.js" //輸出的檔名
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
 }
