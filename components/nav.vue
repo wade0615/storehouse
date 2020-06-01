ee1757cb2337d3bc6c55c37a9c8040f5e1ebc278
Wade <wsw0615@gmail.com>
2020-06-01 10:00:33 +0800

    feat: nav sidebar styles

--- components/nav.vue
+++ components/nav.vue
@@ -2,7 +2,16 @@
     <header class="logo">
         <h1>{{ msg }}</h1>
         <button class="active_sidebar" v-on:click='activeSidebar()'><i class="fas fa-bars"></i></button>
-        <div class="side_bar" :class="{active: isActive}" v-on:click='closeSidebar()'></div>
+        <div class="side_bar" :class="{active: isActive}">
+            <ul>
+                <li v-on:click='closeSidebar()'><span class="icon"><i class="fas fa-backspace"></i></span>返回</li>
+                <li><span class="icon"><i class="fas fa-sign-in-alt"></i></span>登入</li>
+                <li><span class="icon"><i class="fas fa-user-alt"></i></span>註冊</li>
+                <li><span class="icon"><i class="fas fa-language"></i></span>切換語言</li>
+                <li><span class="icon"><i class="fas fa-search-dollar"></i></span>幣別</li>
+            </ul>
+            <div v-on:click.stop='closeSidebar()'></div>
+        </div>
     </header>
 </template>
 
