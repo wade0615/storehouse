1e24679dc209678e507bae44970b9d90dd04d976
Wade <wsw0615@gmail.com>
2020-06-02 15:54:02 +0800

    feat: main.vue & props

--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import nav from './nav.vue'
+import main from './main.vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -9,12 +10,30 @@ Vue.config.productionTip = false //阻止啟動生產訊息
 
 new Vue({
     el: '#app',
+    data: {
+        contents: [
+            {
+                id: 1,
+                title: `I'm props title`
+            },
+            {
+                id: 2,
+                title: `I'm props title`
+            },
+            {
+                id: 3,
+                title: `I'm props title`
+            }
+        ],
+    },
     template: 
     `<div>
         <navbar></navbar>
+        <maincontent v-for="content in contents" :content="content"></maincontent>
     </div>`,
     components: { 
-        navbar: nav
+        navbar: nav,
+        maincontent: main
     },
     mounted : function(){
         console.log('Hello Webpack and Vue !');  
