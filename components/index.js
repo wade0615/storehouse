e0360b4e64538ad9054f7825cc3310da7c064841
Wade <wsw0615@gmail.com>
2020-06-03 16:44:43 +0800

    feat: npm i --save axios vue-axios

--- components/index.js
+++ components/index.js
@@ -4,7 +4,11 @@ import main from './main.vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 Vue.use(BootstrapVue)
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false //阻止啟動生產訊息
 
--- components/main.vue
+++ components/main.vue
@@ -4,11 +4,13 @@
 
         <div>
             <b-card-group deck class="card_group">
-                <b-card v-for="msg in msgs" :title="msg" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+                <b-card v-for="city in cities" :title="city.name" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
                     <b-card-text>
-                        This is a wider card below as a natural lead-in to additional content.
+                        {{ city.weather[0].description }}
+                        <br>
+                        {{ city.main.temp }}
                     </b-card-text>
-                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                    <b-button href="#" variant="primary" @click='mainfunc'>Go somewhere</b-button>
                 </b-card>
             </b-card-group>
         </div>
@@ -20,13 +22,21 @@ export default {
     props: ['content'],
     data () {
         return {
-            msgs: [`Card title 1`,`Card title 2`,`Card title 3`,`Card title 4`,`Card title 5`]
+            msgs: [`Card title 1`,`Card title 2`,`Card title 3`,`Card title 4`,`Card title 5`],
+            cities: []
         }
     },
     methods: {
         mainfunc(){
-            console.log('main.vue active');
+            const api = 'http://api.openweathermap.org/data/2.5/group?id=1668341,1850144,1835848,1609350,1701668&units=metric&appid=77c60ed4e299decbb6b97e90264426ca&lang=en';
+            this.axios.get(api).then((response) => {
+                // console.log(response.data)
+                this.cities = response.data.list
+            });
         }
+    },
+    beforeMount(){
+        this.mainfunc()
     },
 }
 </script>
