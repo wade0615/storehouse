249075cb4ca0fc95d71df0e7a8aea892ecdc9846
Wade <wsw0615@gmail.com>
2020-06-30 16:08:16 +0800

    feat: showPreviewImage with createObjectURL

--- components/customCol.vue
+++ components/customCol.vue
@@ -2,13 +2,13 @@
   <section>
     <h2>{{ title }}</h2>
     <b-button-group>
-      <b-button variant="success" @click='col_6();inner_html();get_file();'>col-6 col-6</b-button>
+      <b-button variant="success" @click='col_6();inner_html();'>col-6 col-6</b-button>
       <b-button variant="info" @click='col_4();inner_html();'>col-4 col-8</b-button>
       <b-button variant="warning" @click='col_3();inner_html();'>col-3 col-9</b-button>
     </b-button-group>
-    <input type="file" id="file_uploader" accept="image/*"></input>
+    <input type="file" id="file_uploader" accept="image/*" @change='get_file();'></input>
     <p id="article" class="row">
-      <span>{{ loremText }}<img id="article_img" class="col-12 col-sm-6" src="https://picsum.photos/300/300/" alt="">
+      <span>{{ loremText }}<img id="article_img" class="col-12 col-sm-6" src="https://picsum.photos/300/300/" alt="" data-target="image-preview">
       {{ loremText }}{{ loremText }}
       </span>
       <span>{{ loremText }}{{ loremText }}</span>
@@ -45,15 +45,21 @@ export default {
         inner_html(){
           console.log('innerHTML')
           let article = document.querySelector('#article')
-          // console.log(article.innerHTML)
+          console.log(article.innerHTML)
         },
         get_file(){
           let data = document.querySelector('#file_uploader')
           console.log(data.value)
+          console.log(data.files[0])
+          this.showPreviewImage(data.files[0])
+        },
+        showPreviewImage(fileObj) {
+          const imagePreview = document.querySelector('[data-target="image-preview"]');
+          const image = URL.createObjectURL(fileObj);
+          imagePreview.src = image;
         }
     },
     beforeMount(){
-        // this.mainfunc()
     },
 }
 </script>
